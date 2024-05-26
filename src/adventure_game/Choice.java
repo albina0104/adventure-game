b9761de2ce5fa1cda5b7c5f2f14040056d97f6ca
Albina Salkayeva <salkaeva.a@gmail.com>
2024-05-26 14:50:59 +0500

    Remove useless imports

--- src/adventure_game/Choice.java
+++ src/adventure_game/Choice.java
@@ -1,7 +1,5 @@
 package adventure_game;
 
-import java.util.function.Consumer;
-
 public abstract class Choice {
 
     public abstract String getDescription();
--- src/adventure_game/Room.java
+++ src/adventure_game/Room.java
@@ -1,7 +1,5 @@
 package adventure_game;
 
-import java.util.function.Consumer;
-
 public abstract class Room extends Choice {
 
     public abstract String getDescription();
